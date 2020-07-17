407887df741d54b3e2c441e16fb3f510d78f50a2
Karol Lewandowski <k.lewandowski.1@research.gla.ac.uk>
2020-07-17 17:52:20 +0200

    my mofem changes

--- var/spack/repos/builtin/packages/med/package.py
+++ var/spack/repos/builtin/packages/med/package.py
@@ -22,7 +22,7 @@ class Med(CMakePackage):
 
     depends_on('mpi')
     depends_on('hdf5@:1.8.19+mpi', when='@3.2.0')
-    depends_on('hdf5@:1.10.2+mpi', when='@4.0.0')
+    depends_on('hdf5@:1.10.6+mpi', when='@4.0.0')
 
     # C++11 requires a space between literal and identifier
     patch('add_space.patch', when='@3.2.0')
