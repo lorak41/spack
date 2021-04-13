0170cec6a234559e7cd7343bb3895c153d4580a2
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgow.ac.uk>
2021-04-13 17:40:20 +0100

    pump to version v0.11.1

--- var/spack/repos/builtin/packages/mofem-cephas/package.py
+++ var/spack/repos/builtin/packages/mofem-cephas/package.py
@@ -17,6 +17,7 @@ class MofemCephas(CMakePackage):
 
     version('develop', branch='develop')
     version('lukasz', branch='lukasz/develop')
+    version('0.11.1', branch='Version0.11.1')
     version('0.11.0', branch='Version0.11.0')
     version('0.10.0', branch='Version0.10.0')
     version('0.9.2', branch='Version0.9.2')
--- var/spack/repos/builtin/packages/mofem-users-modules/package.py
+++ var/spack/repos/builtin/packages/mofem-users-modules/package.py
@@ -18,6 +18,7 @@ class MofemUsersModules(CMakePackage):
 
     version('develop', branch='develop')
     version('lukasz', branch='lukasz/develop')
+    version('0.11.1', branch='Version0.11.1')
     version('0.11.0', branch='Version0.11.0')
     version('0.10.0', branch='Version0.10.0')
     version('0.9.2', branch='Version0.9.2')
@@ -47,6 +48,7 @@ class MofemUsersModules(CMakePackage):
     variant('docker', default=False, description='Build in docker volume')
 
     extends('mofem-cephas')
+    depends_on('mofem-cephas@0.11.1', when='@0.11.1')
     depends_on('mofem-cephas@0.11.0', when='@0.11.0')
     depends_on('mofem-cephas@0.10.0', when='@0.10.0')
     depends_on('mofem-cephas@0.9.2', when='@0.9.2')
