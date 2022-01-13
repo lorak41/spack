a5b4badba133481bc5bf45bc7d7362b9b2bb80f0
likask <likask@wp.pl>
2022-01-13 13:19:34 +0000

    fix petsc dependency

--- var/spack/repos/builtin/packages/mofem-cephas/package.py
+++ var/spack/repos/builtin/packages/mofem-cephas/package.py
@@ -64,12 +64,14 @@ class MofemCephas(CMakePackage):
     depends_on('mpi')
 
     # PETSC install
-    depends_on('petsc@:3.11.99+mumps+mpi')
-    depends_on('slepc@:3.11.99', when='+slepc')
     depends_on('petsc@:3.11.99+mumps+mpi', when='@0.8.7:0.10.0')
     depends_on('slepc@:3.11.99', when='@0.8.7:0.10.0 +slepc')
-    depends_on('petsc@:3.11.99+mumps+mpi', when='@develop')
-    depends_on('slepc@:3.11.99', when='@develop +slepc')
+    depends_on('petsc@:3.14.99+mumps+mpi', when='@0.11.0:')
+    depends_on('slepc@:3.14.99', when='@0.11.0: +slepc')
+    depends_on('petsc@:3.14.99+mumps+mpi', when='@master')
+    depends_on('slepc@:3.14.99', when='@develop +slepc')
+    depends_on('petsc@:3.14.99+mumps+mpi', when='@develop')
+    depends_on('slepc@:3.14.99', when='@develop +slepc')
     depends_on('petsc@:3.14.99+mumps+mpi', when='@lukasz')
     depends_on('slepc@:3.14.99', when='@lukasz +slepc')
   
