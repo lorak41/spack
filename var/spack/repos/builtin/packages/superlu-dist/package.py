8eceac600fb656f8b09499d34ea44533dc280b32
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgow.ac.uk>
2022-01-14 19:49:09 +0000

    fix with big-sur and gfortran-9

--- var/spack/repos/builtin/packages/superlu-dist/package.py
+++ var/spack/repos/builtin/packages/superlu-dist/package.py
@@ -3,6 +3,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import platform
 from spack import *
 
 
@@ -53,6 +54,14 @@ class SuperluDist(CMakePackage, CudaPackage):
 
     patch('xl-611.patch', when='@:6.1.1 %xl')
     patch('xl-611.patch', when='@:6.1.1 %xl_r')
+    
+    @when('%apple-clang')
+    def setup_build_environment(self, env):
+        # Kluge to get the gfortran linker to work correctly on Big
+        # Sur, at least until a gcc release > 10.2 is out with a fix.
+        # (There is a fix in their development tree.)
+        if platform.mac_ver()[0][0:2] == '11':
+            env.set('MACOSX_DEPLOYMENT_TARGET', '10.15') 
 
     def cmake_args(self):
         spec = self.spec
