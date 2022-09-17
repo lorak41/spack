bf81de659c33ceaa2191c88d51367640a8a82e1a
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgowa.ac.uk>
2022-09-17 18:43:03 +0100

    bump petsc

--- var/spack/repos/builtin/packages/mofem-cephas/package.py
+++ var/spack/repos/builtin/packages/mofem-cephas/package.py
@@ -74,10 +74,8 @@ class MofemCephas(CMakePackage):
     depends_on('slepc@:3.11.99', when='@0.8.7:0.10.0 +slepc')
     depends_on('petsc@:3.14.99+mumps+mpi', when='@0.11.0:')
     depends_on('slepc@:3.14.99', when='@0.11.0: +slepc')
-    depends_on('petsc@:3.14.99+mumps+mpi', when='@master')
-    depends_on('slepc@:3.14.99', when='@develop +slepc')
-    depends_on('petsc@:3.14.99+mumps+mpi', when='@develop')
-    depends_on('slepc@:3.14.99', when='@develop +slepc')
+    depends_on('petsc@:3.16.99+mumps+mpi', when='@develop')
+    depends_on('slepc@:3.16.99', when='@develop +slepc')
     depends_on('petsc@:3.16.99+mumps+mpi', when='@lukasz')
     depends_on('slepc@:3.16.99', when='@lukasz +slepc')
   
